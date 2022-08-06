9074103d646ae12c9426a2c59caa183d35b577eb
Ahmad Atef <ahmad.atef.ali.ahmad@gmail.com>
2022-08-06 17:10:28 +0200

    Collect, Collect with count and Map operator

--- 03-transforming-operators/projects/Starter.playground/Contents.swift
+++ 03-transforming-operators/projects/Starter.playground/Contents.swift
@@ -3,36 +3,39 @@ import Combine
 
 var subscriptions = Set<AnyCancellable>()
 
-<#Add your code here#>
+// MARK: Collect
+example(of: "Collect",
+        comment: "waits till the upstream publisher completes, and then collect all published values and send it in a single array to the downstram") {
+    ["A", "B", "C", "D"].publisher
+        .collect()
+        .sink(receiveCompletion: { print($0) },
+              receiveValue: { print($0) })
+        .store(in: &subscriptions)
+}
 
-/// Copyright (c) 2021 Razeware LLC
-///
-/// Permission is hereby granted, free of charge, to any person obtaining a copy
-/// of this software and associated documentation files (the "Software"), to deal
-/// in the Software without restriction, including without limitation the rights
-/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-/// copies of the Software, and to permit persons to whom the Software is
-/// furnished to do so, subject to the following conditions:
-///
-/// The above copyright notice and this permission notice shall be included in
-/// all copies or substantial portions of the Software.
-///
-/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
-/// distribute, sublicense, create a derivative work, and/or sell copies of the
-/// Software in any work that is designed, intended, or marketed for pedagogical or
-/// instructional purposes related to programming, coding, application development,
-/// or information technology.  Permission for such use, copying, modification,
-/// merger, publication, distribution, sublicensing, creation of derivative works,
-/// or sale is expressly withheld.
-///
-/// This project and source code may use libraries or frameworks that are
-/// released under various Open-Source licenses. Use of those libraries and
-/// frameworks are governed by their own individual licenses.
-///
-/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-/// THE SOFTWARE.
+// MARK: Collect with Limit
+example(of: "Collect(count)",
+        comment: "collects the values with the count number you pass, meaning that this operator will wait for the upstream publisher till it finishes and then send the values into the downstream into baches of the count you specifiy") {
+    ["A", "B", "C", "D"].publisher
+        .collect(2)
+        .sink(receiveCompletion: { print($0) },
+              receiveValue: { print($0)})
+        .store(in: &subscriptions)
+}
+
+// MARK: Map
+example(of: "map") {
+    let numberFormatter = NumberFormatter()
+    numberFormatter.locale = .init(languageComponents: .init(language: .init(identifier: "ar")))
+    numberFormatter.numberStyle = .spellOut
+    
+    [123, 4, 56].publisher
+        .map { numberFormatter.string(for: NSNumber(integerLiteral: $0)) ?? "" }
+        .sink(receiveCompletion: { print($0) },
+              receiveValue: { print($0) })
+        .store(in: &subscriptions)
+    
+    // In this block, notice how:
+    // - The NumberFormatter can take different local / language and how the formatter got string from NSNumber Integer
+    // - How the ouptut become non-optional when adding the default value ?? ""
+}
